8ce92d7014ce44843e6f079e30f84f36f6d6b155
Vlad Didenko <vdidenko@getcollc.com>
2009-12-16 23:26:03 -0600

    fix segfault on --clean

--- src/ngx_http_dbrelay_module.c
+++ src/ngx_http_dbrelay_module.c
@@ -95,7 +95,7 @@ ngx_http_dbrelay_exit_master(ngx_cycle_t *cycle)
    for (i=0; i<DBRELAY_MAX_CONN; i++) {
      if (connections[i].sock_path && strlen(connections[i].sock_path)) {
          s = dbrelay_socket_connect(connections[i].sock_path, 2, &error);
-         dbrelay_conn_kill(s);
+         if (s!=-1) dbrelay_conn_kill(s);
      }
      if (connections[i].helper_pid) {
         pid = connections[i].helper_pid;
